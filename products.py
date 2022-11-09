e2f9e28665dd004f8f5c283a8187db82aa503800
Jared Chen <q0915737979@gmail.com>
2022-11-10 00:04:24 +0800

    add read file

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
+# 讀取檔案
 prodcuts = []
+with open('prodcuts.csv', 'r') as f:
+	for line in f: # line 自行定義變數
+		if '商品,價格' in line:
+			continue # 繼續，跳到下一迴的意思
+		name, price = line.strip().split(',')
+		prodcuts.append([name, price])
+print(prodcuts)
+
+# 讓使用者輸入
 while True:
 	name = input('請輸入商品名： ')
 	if name == 'q':
@@ -9,10 +19,12 @@ while True:
 	prodcuts.append([name, price])
 print(prodcuts)
 
+# 印出所有購買記錄
 for p in prodcuts:
 	print(p[0], '的價格是', p[1])
 
-	with open('prodcuts.csv', 'w') as f:
+# 寫入檔案
+with open('prodcuts.csv', 'w') as f:
 		f.write('商品,價格\n')
 		for p in prodcuts:
 			f.write(p[0] + ',' + str(p[1]) + '\n')
